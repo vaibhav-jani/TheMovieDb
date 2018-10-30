0a21f27605744e206aa15b0d9fc5232c2c167360
Vaibhav <vaibhav.jani@byjus.com>
2018-10-30 18:25:05 +0530

    Updated gradle dependencies and minor fix

--- app/src/main/java/com/themoviedb/apis/retrofit/AppUrls.java
+++ app/src/main/java/com/themoviedb/apis/retrofit/AppUrls.java
@@ -4,5 +4,5 @@ public class AppUrls {
 
     public static final String BASE_URL = "https://api.themoviedb.org/3/";
     public static final String IMAGES_BASE_URL = "http://image.tmdb.org/t/p/w300";
-    public static final String LARGER_IMAGES_BASE_URL = "http://image.tmdb.org/t/p/w600";
+    public static final String LARGER_IMAGES_BASE_URL = "http://image.tmdb.org/t/p/w300";
 }
